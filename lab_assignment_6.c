ba1a260eaeaf0dd0127e59610873f281c148501b
surxer <saoud.alquri@knights.ucf.edu>
2023-06-23 14:15:25 -0400

    lab 6

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,7 +2,23 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+if (low > high) 
+	
+return -1;
+
+ int mid = (low+high)/2;
+
+ if (value < array[mid])
+
+ return search(numbers, low, mid-1, value);
+
+ else if (value > array[mid])
+
+ return search(numbers, mid+1, high, value);
+
+ else
+
+ return mid;
 }
 
 void printArray(int numbers[], int sz)
